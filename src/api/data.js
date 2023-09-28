de6a5b96e68de33605638345b08f8c384a2d7147
AGN <itzme907@gmail.com>
2023-09-28 15:01:26 +0300

    add data and images for the api

---
+++ src/api/data.js
@@ -0,0 +1,48 @@
+import donKanonji from '../assets/images/donkanonji.png'
+import eishima from '../assets/images/eishima.png'
+import fishboneD from '../assets/images/fishboneD.png'
+import gabrielli from '../assets/images/gabrielli.png'
+import giselle from '../assets/images/giselle.png'
+import grandFisher from '../assets/images/grandfisher.png'
+import ichigo from '../assets/images/ichigo.png'
+import kajomaru from '../assets/images/kajomaru.png'
+import keigo from '../assets/images/keigo.png'
+import kotsubaki from '../assets/images/kotsubaki.png'
+import redder from '../assets/images/redder.png'
+import tsukishima from '../assets/images/tsukishima.png'
+import makizo from '../assets/images/makizo.png'
+import taketsuna from '../assets/images/taketsuna.png'
+import hiyosu from '../assets/images/hiyosu.png'
+import diRoy from '../assets/images/diRoy.png'
+import gigo from '../assets/images/gigo.png'
+import ebern from '../assets/images/ebern.png'
+import luders from '../assets/images/luders.png'
+import izumi from '../assets/images/izumi.png'
+import kanae from '../assets/images/kanae.png'
+
+const allCharacters = [
+  { id: 0, name: 'Don Kanonji', image: donKanonji },
+  { id: 1, name: 'Shinobu Eishima', image: eishima },
+  { id: 2, name: 'Fishbone D', image: fishboneD },
+  { id: 3, name: 'Berenice Gabrielli', image: gabrielli },
+  { id: 4, name: 'Giselle Gewelle', image: giselle },
+  { id: 5, name: 'Grand Fisher', image: grandFisher },
+  { id: 6, name: 'Kurosaki Ichigo', image: ichigo },
+  { id: 7, name: 'Hidetomo Kajōmaru', image: kajomaru },
+  { id: 8, name: 'Keigo Asano', image: keigo },
+  { id: 9, name: 'Jinemon Kotsubaki', image: kotsubaki },
+  { id: 10, name: 'Abirama Redder', image: redder },
+  { id: 11, name: 'Shūkurō Tsukishima', image: tsukishima },
+  { id: 12, name: 'Makizō Aramaki', image: makizo },
+  { id: 13, name: 'Taketsuna Gori', image: taketsuna },
+  { id: 14, name: 'Hiyosu', image: hiyosu },
+  { id: 15, name: 'Di Roy Rinker', image: diRoy },
+  { id: 16, name: 'Gigo Vega', image: gigo },
+  { id: 17, name: 'Asguiaro Ebern', image: ebern },
+  { id: 18, name: 'Luders Friegen', image: luders },
+  { id: 19, name: 'Izumi Ishida', image: izumi },
+  { id: 20, name: 'Kanae Katagiri', image: kanae },
+]
+
+
+export default allCharacters
